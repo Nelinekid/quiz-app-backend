c1734fb93874b3a60469c6239ae2b345059df5d7
Gambi18 <charlestima23@gmail.com>
2024-09-05 12:05:37 +0100

    Addition of .env

--- config/db.go
+++ config/db.go
@@ -2,8 +2,11 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/Gambi18/Quizzo/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,7 +15,17 @@ var DB *gorm.DB
 
 // Connect to PostgreSQL database and return the connection object
 func Connect() {
-	db, err := gorm.Open(postgres.Open("postgres://postgres:gorm2037@localhost:5432/quizzo"), &gorm.Config{})
+	// Find .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	//define env
+	DB_URL := os.Getenv("DB_URL")
+	DB_nURL := os.Getenv("DB_nURL")
+
+	db, err := gorm.Open(postgres.Open(DB_URL), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Failed to connect to database server:", err)
 		return
@@ -20,7 +33,7 @@ func Connect() {
 
 	// Create the database if it doesn't exist
 	dbName := "quizzo"
-    createDBSQL := fmt.Sprintf(`
+	createDBSQL := fmt.Sprintf(`
     DO $$
     BEGIN
         IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '%s') THEN
@@ -29,13 +42,13 @@ func Connect() {
     END
     $$;`, dbName, dbName)
 
-    if err := db.Exec(createDBSQL).Error; err != nil {
-        fmt.Println("Failed to create database:", err)
-        return
-    }
+	if err := db.Exec(createDBSQL).Error; err != nil {
+		fmt.Println("Failed to create database:", err)
+		return
+	}
 
 	// Connect to the new database
-	dsnWithDB := fmt.Sprintf("host=localhost user=postgres password=gorm2037 dbname=%s sslmode=disable", dbName)
+	dsnWithDB := fmt.Sprintf(DB_nURL, dbName)
 	db, err = gorm.Open(postgres.Open(dsnWithDB), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Failed to connect to the specific database:", err)
@@ -43,10 +56,10 @@ func Connect() {
 	}
 
 	// Enable uuid-ossp extension
-    if err := db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";").Error; err != nil {
-        fmt.Println("Failed to enable uuid-ossp extension:", err)
-        return
-    }
+	if err := db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";").Error; err != nil {
+		fmt.Println("Failed to enable uuid-ossp extension:", err)
+		return
+	}
 
 	DB = db
 	db.AutoMigrate(&models.User{})
